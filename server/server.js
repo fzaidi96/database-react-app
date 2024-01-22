3d9ab0008e8ee271225bf4dea8e99a615a380850
fzaidi96 <f.zaidi182@gmail.com>
2024-01-22 10:40:21 +0000

    amend errors second time

--- server/server.js
+++ server/server.js
@@ -10,15 +10,15 @@ const db = new pg.Pool({ connectionString: dbConnectionString });
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.listen("https://database-react-app-server.onrender.com", function () {
-    console.log("Server listening");});
+app.listen(8080,, function () {
+    console.log("Server listening at 8080");});
 
-app.get("/", async (request, response) => {
+app.get("https://database-react-app-server.onrender.com", async (request, response) => {
   const result = await db.query("SELECT * FROM message_board ");
   response.json(result.rows);
   });
   
-app.post("/", async (request, response) => {
+app.post("https://database-react-app-server.onrender.com", async (request, response) => {
   const username = request.body.username;
   const post = request.body.post;
   const topic = request.body.topic;
@@ -27,7 +27,7 @@ app.post("/", async (request, response) => {
   response.json(newPost.rows[0]);
   });
   
-  app.delete("/:id", async (request, response) => {
+  app.delete("https://database-react-app-server.onrender.com/:id", async (request, response) => {
     const recordId = request.params.id;
     const result = await db.query("DELETE FROM message_board WHERE id = $1", [recordId]);
     response.json(result.rows[0]);
