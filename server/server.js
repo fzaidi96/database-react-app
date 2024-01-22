fbc96c5fb40562b6013b4e40124aaed9b877a690
fzaidi96 <f.zaidi182@gmail.com>
2024-01-22 10:47:24 +0000

    amend errors 4

--- server/server.js
+++ server/server.js
@@ -13,12 +13,12 @@ app.use(cors());
 app.listen(8080, function () {
     console.log("Server listening at 8080");});
 
-app.get("https://database-react-app-server.onrender.com", async (request, response) => {
+app.get("/", async (request, response) => {
   const result = await db.query("SELECT * FROM message_board ");
   response.json(result.rows);
   });
   
-app.post("https://database-react-app-server.onrender.com", async (request, response) => {
+app.post("/", async (request, response) => {
   const username = request.body.username;
   const post = request.body.post;
   const topic = request.body.topic;
@@ -27,7 +27,7 @@ app.post("https://database-react-app-server.onrender.com", async (request, respo
   response.json(newPost.rows[0]);
   });
   
-  app.delete("https://database-react-app-server.onrender.com/:id", async (request, response) => {
+  app.delete("/:id", async (request, response) => {
     const recordId = request.params.id;
     const result = await db.query("DELETE FROM message_board WHERE id = $1", [recordId]);
     response.json(result.rows[0]);
